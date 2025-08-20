f9ff8f814734855348dfe4ca08c148f73d7e0bb3
josevaldirdev <josevaldirdev@gmail.com>
2025-08-20 14:39:00 -0300

    props

--- src/components/ProductDetailsCard/index.tsx
+++ src/components/ProductDetailsCard/index.tsx
@@ -1,11 +1,11 @@
-import './style.css'
+import "./style.css";
 
-import ProductCategory from '../ProductCategory'
-import { ProductDTO } from '../../models/product'
+import ProductCategory from "../ProductCategory";
+import { ProductDTO } from "../../models/product";
 
 type Props = {
   product: ProductDTO;
-}
+};
 
 export default function ProductDetailsCard({ product }: Props) {
   return (
@@ -16,17 +16,11 @@ export default function ProductDetailsCard({ product }: Props) {
       <div className="dsc-product-details-bottom">
         <h3>R$ {product.price.toFixed(2)}</h3>
         <h4>{product.name}</h4>
-        <p>
-          {product.description}
-        </p>
+        <p>{product.description}</p>
         <div className="dsc-category-container">
-          {
-            product.categories.map(item => (
-              <ProductCategory name={item.name} />
-            ))
-          }
-
-
+          {product.categories.map((item) => (
+            <ProductCategory key={item.id} name={item.name} />
+          ))}
         </div>
       </div>
     </div>
--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -2,7 +2,31 @@ import ButtonNextPage from '../../components/ButtonNextPage';
 import CatalogCard from '../../components/CatalogCard';
 import HeaderClient from '../../components/HeaderClient'
 import SearchBar from '../../components/SearchBar';
+import { ProductDTO } from '../../models/product';
 import './styles.css'
+
+const product: ProductDTO = {
+  id: 2,
+  name: "Smart TV",
+  description: "Esta TV é muito bonita",
+  imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
+  price: 2500.99,
+  categories: [
+    {
+      id: 2,
+      name: "Eletrônicos"
+    },
+    {
+      id: 3,
+      name: "Computadores"
+    },
+    {
+      id: 4,
+      name: "Importados"
+    }
+  ]
+
+}
 
 export default function Catalog() {
   return (
@@ -14,17 +38,18 @@ export default function Catalog() {
           <SearchBar />
 
           <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+
           </div>
 
           <ButtonNextPage />
