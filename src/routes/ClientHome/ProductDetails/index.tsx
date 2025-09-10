aa7a8d2356343b4d5cb63b9a8b82080511b57ee8
josevaldirdev <josevaldirdev@gmail.com>
2025-09-10 15:14:21 -0300

    Capturando erros de requisição

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -3,27 +3,33 @@ import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import * as productService from "../../../services/product-service";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
 
 export default function ProductDetails() {
   const params = useParams();
+  const navigate = useNavigate();
 
   /* essa linha começa o product como undefined e é definido o typo com ProductDTO */
   const [product, setProduct] = useState<ProductDTO>();
 
   useEffect(() => {
-    productService.findById(Number(params.productId)).then((response) => {
-      console.log(response.data);
-      setProduct(response.data);
-    });
+    productService
+      .findById(Number(params.productId))
+      .then((response) => {
+        setProduct(response.data);
+      })
+      .catch(() => {
+        navigate("/catalog");
+      });
   }, []); /* recebe dois argumentos, uma função e uma lista que ele vai observar, faz requisições no back-end com axios */
 
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
         {product && <ProductDetailsCard product={product} />}
+
         <div className="dsc-btn-page-container">
           <ButtonPrimary text="Comprar" />
           <Link to="/">
