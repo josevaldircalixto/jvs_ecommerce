2553fea8a5a827e03e4d33999c2b78422477f545
josevaldirdev <josevaldirdev@gmail.com>
2025-09-10 14:57:53 -0300

    Primeiras requisições do back-end

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -1,17 +1,28 @@
 import "./styles.css";
 import ButtonNextPage from "../../../components/ButtonNextPage";
+import * as productService from "../../../services/product-service";
 import CatalogCard from "../../../components/CatalogCard";
 import SearchBar from "../../../components/SearchBar";
-import * as productService from "../../../services/product-service";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product";
 
 export default function Catalog() {
+  /* inicia com uma lista vazia de produtos por isso productDTO tem uma lista vazia */
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+
+  useEffect(() => {
+    productService.findAll().then((response) => {
+      setProducts(response.data.content);
+    });
+  }, []);
+
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
         <SearchBar />
 
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-          {productService.findAll().map((product) => (
+          {products.map((product) => (
             <CatalogCard key={product.id} product={product} />
           ))}
         </div>
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -3,12 +3,23 @@ import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import * as productService from "../../../services/product-service";
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product";
 
 export default function ProductDetails() {
   const params = useParams();
-  const product = productService.findById(Number(params.productId)); //pega o conteúdo do prductId para colocar renderizar na tela
+
+  /* essa linha começa o product como undefined e é definido o typo com ProductDTO */
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    productService.findById(Number(params.productId)).then((response) => {
+      console.log(response.data);
+      setProduct(response.data);
+    });
+  }, []); /* recebe dois argumentos, uma função e uma lista que ele vai observar, faz requisições no back-end com axios */
+
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
